8d18c1da7c027e948d0025e15843929b940e51b8
Chris Religa <chris.religa@gmail.com>
2026-01-06 20:46:43 -0800

    Updated the UX so only one accordion item is displayed at a time

--- Frontend Projects/13-accordion/script.js
+++ Frontend Projects/13-accordion/script.js
@@ -7,6 +7,12 @@ tabs.forEach((tab) => {
   const answerTab = tab.querySelector('.answer');
 
   openTab.addEventListener('click', () => {
+    const openAnswer = document.querySelector('.answer.active');
+
+    if (openAnswer && openAnswer !== answerTab) {
+      openAnswer.classList.remove('active');
+    }
+
     answerTab.classList.toggle('active');
   });
 });
